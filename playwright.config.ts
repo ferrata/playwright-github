791c592c4216e540840f97389bc1fb28a65a55b7
Sasha Goloshchapov <ferrata@hey.com>
2024-12-22 16:08:04 -0600

    feat: test ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
+const testUrl = process.env.TEST_URL ?? 'http://localhost:3001';
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -25,8 +27,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: testUrl,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,11 @@
 import { test, expect } from '@playwright/test';
+import { strict } from 'assert';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('has title', async ({ baseURL, page }) => {
+  strict(baseURL, 'baseURL is required')  
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await page.goto(baseURL);
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page).toHaveTitle(/Create Next App/);
+  await expect(page).toHaveScreenshot();
 });
